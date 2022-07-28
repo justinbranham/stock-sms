dac6ec3e89963224dcc7a69d800b518801684c38
Justin <justin@Justins-MacBook-Pro.local>
2022-07-27 22:47:58 -0400

    setup cronjob

--- app.js
+++ app.js
@@ -2,13 +2,246 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
 const app = express();
+const twilio = require('twilio');
+const CronJob = require('cron').CronJob;
+const dotenv = require('dotenv');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+dotenv.config();
+
+
+const client=  twilio(process.env.ACCOUNTSID,process.env.AUTHTOKEN);
+
+
+const jobSpy = new CronJob(
+  '1 16 * * *',
+  function(){
+    request.get({
+      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&apikey='+ process.env.ALPHAVANTAGEKEY,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is successfully parsed as a JSON object
+        const close = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+        client.messages.create ({
+          to: process.env.YOURPHONENUMBER,
+          from: process.env.YOURTWILIOPHONENUMBER,
+          body: 'SPY ' + close
+        });
+      }
+    });
+    
+  }, 
+null,
+true,
+'America/New_York'
+);
+
+const jobBaba = new CronJob(
+  '1 16 * * *',
+  function(){
+    request.get({
+      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BABA&apikey='+ process.env.ALPHAVANTAGEKEY,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is successfully parsed as a JSON object
+        const close = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+        client.messages.create ({
+          to: process.env.YOURPHONENUMBER,
+          from: process.env.YOURTWILIOPHONENUMBER,
+          body: 'BABA ' + close
+        });
+      }
+    });
+    
+  }, 
+  null,
+  true,
+  'America/New_York'
+);
+
+const jobMnkd = new CronJob(
+  '1 16 * * *',
+  function(){
+    request.get({
+      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MNKD&apikey='+ process.env.ALPHAVANTAGEKEY,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is successfully parsed as a JSON object
+        const close = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+        client.messages.create ({
+          to: process.env.YOURPHONENUMBER,
+          from: process.env.YOURTWILIOPHONENUMBER,
+          body: 'MNKD ' + close
+        });
+      }
+    });
+    
+  }, 
+null,
+true,
+'America/New_York'
+);
+
+const jobFtrp = new CronJob(
+  '1 16 * * *',
+  function(){
+    request.get({
+      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FTRP&apikey='+ process.env.ALPHAVANTAGEKEY,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is successfully parsed as a JSON object
+        const close = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+        client.messages.create ({
+          to: process.env.YOURPHONENUMBER,
+          from: process.env.YOURTWILIOPHONENUMBER,
+          body: 'FTRP ' + close
+        });
+      }
+    });
+    
+  }, 
+null,
+true,
+'America/New_York'
+);
+
+const jobIcpt = new CronJob(
+  '1 16 * * *',
+  function(){
+    request.get({
+      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=ICPT&apikey='+ process.env.ALPHAVANTAGEKEY,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is successfully parsed as a JSON object
+        const close = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+        client.messages.create ({
+          to: process.env.YOURPHONENUMBER,
+          from: process.env.YOURTWILIOPHONENUMBER,
+          body: 'ICPT ' + close
+        });
+      }
+    });
+    
+  }, 
+null,
+true,
+'America/New_York'
+);
+
+
+
+
+
+// function getBabaData(){
+//         const babaUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BABA&apikey='+ process.env.ALPHAVANTAGEKEY;
+
+//         request.get({
+//             url: babaUrl,
+//             json: true,
+//             headers: {'User-Agent': 'request'}
+//           }, (err, res, data) => {
+//             if (err) {
+//               console.log('Error:', err);
+//             } else if (res.statusCode !== 200) {
+//               console.log('Status:', res.statusCode);
+//             } else {
+//               // data is successfully parsed as a JSON object:
+//               const babaClose = String(Object.values(Object.values(Object.values(data)[1])[0])[3]);
+//               console.log(babaClose)
+//               return babaClose;
+//             }
+//         });
+// }
+
+// let getBabaDataOutput = getBabaData();
+// console.log(getBabaDataOutput);
+
+
+
+
+
+        // const mnkdUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MNKD&apikey='+ process.env.ALPHAVANTAGEKEY;
+// 
+        // request.get({
+        //     url: mnkdUrl,
+        //     json: true,
+        //     headers: {'User-Agent': 'request'}
+        //   }, (err, res, data) => {
+        //     if (err) {
+        //       console.log('Error:', err);
+        //     } else if (res.statusCode !== 200) {
+        //       console.log('Status:', res.statusCode);
+        //     } else {
+        //       // data is successfully parsed as a JSON object:
+        //       const mnkdClose = Object.values(Object.values(Object.values(data)[1])[0])[3];
+        //       //console.log(mnkdClose);
+        //     }
+        // });
+
+        // const spyUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&apikey='+ process.env.ALPHAVANTAGEKEY;
+
+        // request.get({
+        //     url: spyUrl,
+        //     json: true,
+        //     headers: {'User-Agent': 'request'}
+        //   }, (err, res, data) => {
+        //     if (err) {
+        //       console.log('Error:', err);
+        //     } else if (res.statusCode !== 200) {
+        //       console.log('Status:', res.statusCode);
+        //     } else {
+        //       // data is successfully parsed as a JSON object:
+        //       const spyClose = Object.values(Object.values(Object.values(data)[1])[0])[3];
+        //       //console.log(spyClose);
+        //       return spyClose;
+        //     }
+        // });
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/signup.html');
